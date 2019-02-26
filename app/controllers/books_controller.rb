55f2e3cda2e03c96d10b4b290e000ee990097b73
williamsc51 <williams.a.courtney@gmail.com>
2019-02-26 11:15:02 -0500

    Add CRUD action/views for books and users

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,48 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = 5
+    if @book.save
+      redirect_to(books_path)
+    else
+      render('new')
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+
+    if @book.update_attributes(book_params)
+      redirect_to(book_path(@book))
+    else
+      render('edit')
+    end
+
   end
 
   def delete
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn, :price, :description, :condition, :thumbnail, :category)
   end
 
 end
