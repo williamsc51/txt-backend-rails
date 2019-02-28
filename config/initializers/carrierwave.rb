cee363ffe489f402bd1fb59704581380d790d482
williamsc51 <williams.a.courtney@gmail.com>
2019-02-28 17:36:33 -0500

    update carrier wave. remove fetch() from ENV

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,6 @@
-if Rails.env.production? or Rails.env.development?
 CarrierWave.configure do |config|
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
+    config.aws_bucket = ENV['S3_BUCKET_NAME'] # for AWS-side bucket access permissions config, see section below
     config.aws_acl    = 'public-read'
   
     # Optionally define an asset host for configurations that are fronted by a
@@ -31,5 +30,4 @@ CarrierWave.configure do |config|
     # config.aws_signer = -> (unsigned_url, options) do
     #   Aws::CF::Signer.sign_url(unsigned_url, options)
     # end
-end
 end
