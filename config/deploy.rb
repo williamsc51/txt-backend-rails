3fcd6be70fe4cf8d0bc21fb119d3c72544c65f21
williamsc51 <williams.a.courtney@gmail.com>
2019-02-28 22:59:15 -0500

    link shared secret file

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rvm_map_bins, %w{gem rake ruby rails bundle}
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+ append :linked_files, "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
