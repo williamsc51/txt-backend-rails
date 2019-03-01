288648f24d20bd080d1cce24b9c8deade68c55eb
williamsc51 <williams.a.courtney@gmail.com>
2019-02-28 22:14:14 -0500

    update application name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "txttide"
+set :application, "txt-backend-rails"
 set :repo_url, "git@github.com:williamsc51/txt-backend-rails.git"
 
 
