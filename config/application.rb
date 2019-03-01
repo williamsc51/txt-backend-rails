4897f52fbb45d9c030622bf9d14b54e5f59336a5
williamsc51 <williams.a.courtney@gmail.com>
2019-02-28 19:30:14 -0500

    disable credentials

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module TxtBackendRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.require_master_key = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
