7113bcd7c085d4ee971f12b2d72339dd9e3df665
williamsc51 <williams.a.courtney@gmail.com>
2019-04-04 23:13:48 -0400

    added action mailer for welcome email on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      UserMailer.with(user: @user).welcome_email.deliver_now
       flash[:notice] = "User successfully added"
       redirect_to(users_path)
     else
