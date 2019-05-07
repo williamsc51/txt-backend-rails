53769785ccb1a80970e07652e289d1ced33a3de1
williamsc51 <williams.a.courtney@gmail.com>
2019-05-07 15:35:08 -0400

    update env data and scope for mailer

--- app/resources/api/v1/user_resource.rb
+++ app/resources/api/v1/user_resource.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserResource < JSONAPI::Resource
   attributes :fname, :lname, :email, :password
-  after_save :new_user_mailer
+  after_save :new_user_mailer, on: :create
   has_many :books
 
   def fetchable_fields
