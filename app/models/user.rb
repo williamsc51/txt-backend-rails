3621a7433c633744edffbfb987b38f9c72fc613f
williamsc51 <williams.a.courtney@gmail.com>
2019-05-07 15:35:46 -0400

    added Rubocop for linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# User model
 class User < ApplicationRecord
+  validates :fname, :lname, presence: true, length: { minimum: 2 }
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
