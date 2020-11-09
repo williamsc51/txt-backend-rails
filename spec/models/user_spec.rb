80434ba1bbbdbad4820d4903298ee12623f96638
Courtney Williams <williams.a.courtney@gmail.com>
2020-11-09 17:46:47 -0500

    ran annotate to update model annotations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,14 @@
 #
 # Table name: users
 #
-#  id                     :bigint           not null, primary key
-#  fname                  :string
-#  lname                  :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  authentication_token   :string(30)
-#  admin                  :boolean          default(FALSE)
+#  id              :bigint           not null, primary key
+#  fname           :string
+#  lname           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  email           :string           default(""), not null
+#  admin           :boolean          default(FALSE)
+#  password_digest :string
 #
 
 require 'rails_helper'
