c8aa4d026e0c60170f3c0fc461bc015f331c20a8
Courtney Williams <williams.a.courtney@gmail.com>
2020-11-24 01:39:24 -0500

    remove cover model and drop table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_074553) do
+ActiveRecord::Schema.define(version: 2020_11_24_063726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,12 +69,6 @@ ActiveRecord::Schema.define(version: 2020_11_22_074553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "books_count", default: 0, null: false
-  end
-
-  create_table "covers", force: :cascade do |t|
-    t.string "cover"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "oauth_access_grants", force: :cascade do |t|
