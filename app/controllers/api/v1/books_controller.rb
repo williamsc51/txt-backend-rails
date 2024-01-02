bec63aa2d0daec63206d8c19ae0ca68ca6646b28
Courtney Williams <williams.a.courtney@gmail.com>
2024-01-02 17:49:20 -0500

    update books api controller to only return index and show to frontend

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,51 +1,18 @@
 class Api::V1::BooksController < ApplicationController
 
-  before_action :check_logged_in, only: [:create, :update]
-  before_action :is_owner?, only: [:update]
-
   def index
     @books = BookFilters.apply(params)
     render json: @books
   end
 
-  def create
-    @book = Book.new(book_params.merge({user: current_user}))
-
-    if @book.save
-      render json: @book, status: :created
-    else
-      render json: {logged_in: false, message: "user needs to be authenticated", errors: @book.errors, status: :unprocessable_entity}
-    end
-  end
-
   def show
-    @book = book
-    render json: @book
-  end
-
-  def update
-    @book = book
-
-    if @book.update(book_params)
-      render json: @book
-    else
-      render json: @book.errors
-    end
+    render json: book
   end
 
   private
 
   def book
     @book ||= Book.find(params[:id])
-  end
-
-  def is_owner?
-    if current_user.id == book.user_id
-      true
-    else
-      render json: { status: :unauthorized }
-      false
-    end
   end
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
-      resources :books
+      resources :books, only: [:index, :show]
       resources :users
       resource :cart
       resources :books_carts
